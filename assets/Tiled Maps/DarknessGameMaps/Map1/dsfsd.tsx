3e6791f46f0ee838161b0881b557ec4669a0dafc
Rye Pankoski <100056898+RyePankoski@users.noreply.github.com>
2024-11-20 01:54:48 -0700

    Tiled
    Added tiled specific files to assets

---
+++ assets/Tiled Maps/DarknessGameMaps/Map1/dsfsd.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="dsfsd" tilewidth="64" tileheight="64" tilecount="357" columns="21">
+ <image source="Space station._32_2.jpg" width="1344" height="1088"/>
+</tileset>
